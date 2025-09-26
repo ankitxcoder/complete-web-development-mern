802a468589d16c58ac20c6f27be6a7a1222ccb36
ankitxcoder <ankitone02@gmail.com>
2025-09-26 09:00:57 +0530

    Adding javaScript file

--- frontend/javaScript/part_05.md
+++ frontend/javaScript/part_05.md
@@ -34,4 +34,47 @@
 - square ---> ydi ek variable bnaya th object[variable] dala th uska length aa jayega 
 - dot --> upr wala ni hoga --> js ko smjh me ni ayega
 
-# 
+# Get Values --> js Converts ?
+- js automatically converts Object ( Keys ) --> to String 
+- if we made (Number) as a (Key ) --> the number will converted to ( String )
+- keywords in object ---> converted to ( String ) by js 
+
+# Add / UPdate / Delete Value
+- ObjectLiteral.Key = "Value";  --> this will update the Object literals
+- ObjectLiteral[key] = "Value";  --> update the object literal
+
+- delete objectLiteral.key ---> that key will be deleted
+- delete ObjectLiteral[key]  ---> this will delete the Key value
+
+# ( Nested Object ) Object Of Objects 
+- here const object will be object( {} )
+- eg: parrent object --> fir unka bacha ( objects )
+-  const classInfo = {
+    ankit: {
+        name : "ANkit",
+        age:24
+    },
+    ankita : {
+        name : "ANkita ",
+        age : 20
+    }
+}
+
+# Array Of Object
+- here const object will be Array
+- const Object = [];
+- const object = [ {},{},{} ];
+
+# Math Object
+- some constant unit are Pi and E ( Math.PI  and Math.E )
+### Method in math object
+- Math.abs(-12); ---> gives Absolute value --> i.e gives non +ve and non -ve number we get 
+- Math.pow(2,4); --> 2 ki power 4
+- Math.floor(2.5);  --> 2 answer --> km wale number ke trf round off mtlb 2 ke trf round hoga
+- nearest integer ROund off hoga
+- Math.ceil(n)  ---> larger interger value pr round off kr dega
+### Math.random()  --> important
+- Math.random()  --> 0 to 1 ( here 1 kbhi ni dega its excluded )  --> 0 se 1 ke bech
+
+# Random Integer
+- generate random numnber between fixed number
--- frontend/javaScript/part_05_Code.js
+++ frontend/javaScript/part_05_Code.js
@@ -43,3 +43,77 @@ post[vari];
 console.log(post[vari]);
 console.log(post.vari);     //----------------> Dobut here 
 
+// JS converts KEy to string 
+
+const obj= {
+    1:"a",
+    2:"df",
+    null:"e",
+    true:"ank",   
+    undefined:1
+}
+ console.log(obj[1]);   //----> result ia a ( It Means js converted 1 as string not 1 as index ) 
+// here null , true , undefined --> are key -- but js --> convert it to (String) along with 1,2
+
+// Add Update Value
+const studentAddUpdate = {
+    name:"Ankit",
+    age: 24,
+    marks: 94.5,
+    city:"noida"
+};
+console.log(studentAddUpdate.name="Ankita Kumari");
+console.log(studentAddUpdate["age"]="An");
+console.log("After changing value of age name:", + studentAddUpdate);
+console.log(studentAddUpdate);
+
+// delete name from object Literal
+console.log(delete studentAddUpdate.name );
+console.log(studentAddUpdate);
+
+// Object of Object 
+const classinfo = {
+    ankit : {
+        name:"Ankit kushwaha",
+        age:20
+    },
+    aman: {
+        name : "Aman kumar",
+        age: 20
+    }
+};
+console.log(classinfo);
+console.log(classinfo.ankit);
+console.log(classinfo.ankit.name);
+// changing nestead object value 
+console.log(classinfo.ankit.name="ankit name changed here ");
+
+// Array Of Object
+const classinfoSecond=[
+    {
+        name: "Ankit in Array of object",
+        age : 20
+    },
+    {
+        name: "another name of object",
+        age : 21
+    },
+    {
+        name: "3rd Name Of Object",
+        age : 22
+    }
+]
+console.log(classinfoSecond);
+
+// accessing elemnt by index wise 
+console.log(classinfoSecond[1].name);
+console.log(classinfoSecond[0].name);
+console.log(classinfoSecond[1].gender="sixt index data");
+console.log(classinfoSecond);
+
+//math method 
+console.log(Math.abs(-12));
+console.log(Math.pow(2,2));
+console.log(Math.floor(-5.9));
+console.log(Math.ceil(-5.9));
+console.log(Math.random());
