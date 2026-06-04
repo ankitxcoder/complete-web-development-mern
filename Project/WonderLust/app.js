96489d789295beb469fdb417687b2af084211775
AnkitX <ankitone02@gmail.com>
2026-06-04 11:31:36 +0530

    Learning CRUD opertaion

--- Project/WonderLust/app.js
+++ Project/WonderLust/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URl = "mongodb://127.0.0.1:27017/test";
 main()
@@ -20,6 +21,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("HomePage route");
@@ -48,9 +50,37 @@ app.get("/listings/:id", async (req, res) => {
 
 // Create Post Route
 app.post("/listings", async (req, res) => {
-  // let {title,description,image,price,country,location}= req.body;
-  let listing = req.body.listing;
-  console.log(listing);
+  // req.body.listing --> raw data with no rules
+  // // let {title,description,image,price,country,location}= req.body;
+  // let listing = req.body.listing;
+  // console.log(listing);
+  // now need to convert this data into mongoose documnet so it can validate and do etc etc
+  const newlistong = new Listing(req.body.listing);
+  await newlistong.save();
+  res.redirect("/listingsss");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listingsIdData = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listingsIdData });
+});
+
+//update Rote
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  // res.redirect("/listingsss");
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listingsss");
 });
 // app.get("/testList", async (req, res) => {
 //   let sampleListening = new Listing({
